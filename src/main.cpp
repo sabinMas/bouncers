eb682a89161413afad54140d06fd7fe3a3a107f8
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-02-23 19:03:00 +0000

    added y boundaries for 2 dimensional movement

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,11 @@ static constexpr int HALF_SCREEN_WIDTH = bn::display::width() / 2;
 static constexpr bn::fixed MIN_X = -HALF_SCREEN_WIDTH;
 static constexpr bn::fixed MAX_X = HALF_SCREEN_WIDTH;
 
+// the Y screen boundaries for two dimensional movement
+static constexpr int HALF_SCREEN_HEIGHT = bn::display::height() / 2;
+static constexpr bn::fixed MIN_Y = -HALF_SCREEN_HEIGHT;
+static constexpr bn::fixed MAX_Y = HALF_SCREEN_HEIGHT;
+
 // Starting speed of a bouncer
 static constexpr bn::fixed BASE_SPEED = 2;
 
@@ -25,13 +30,17 @@ class Bouncer
 public:
     bn::sprite_ptr sprite = bn::sprite_items::dot.create_sprite();
     bn::fixed x_speed = BASE_SPEED;
+    bn::fixed y_speed = BASE_SPEED;
 
     void update()
     {
         bn::fixed x = sprite.x();
+        bn::fixed y = sprite.y();
 
         // Update x position by adding speed
         x += x_speed;
+        // Update y position by adding speed
+        y += y_speed;
 
         // If we've gone off the screen on the right
         if (x > MAX_X)
@@ -48,7 +57,20 @@ public:
             x_speed *= -1;
         }
 
+        // update the Y postition and reverse direction if we've gone off the screen
+        if (y > MAX_Y)
+        {
+            y = MAX_Y;
+            y_speed *= -1;
+        }
+        if (y < MIN_Y)
+        {
+            y = MIN_Y;
+            y_speed *= -1;
+        }
+
         sprite.set_x(x);
+        sprite.set_y(y);
     }
 };
 
